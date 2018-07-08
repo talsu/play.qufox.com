bf61f963aed08feb511f8a58e4e68bb557aad259
talsu <talsu84@gmail.com>
2018-07-08 23:36:53 +0900

    Add keyboard bind.

--- js/block.js
+++ js/block.js
@@ -2,6 +2,8 @@ game.Block = me.Entity.extend({
   init : function (x, y) {
     this._super(me.Entity, "init", [x, y, { width: 20, height: 20 }]);
 
+    this.dropVelocity = 20;
+
     this.renderable = new (me.Renderable.extend({
         init : function () {
             this._super(me.Renderable, "init", [0, 0, 20, 20]);
@@ -10,18 +12,46 @@ game.Block = me.Entity.extend({
         draw : function (renderer) {
             renderer.setColor('#5EFF7E');
             renderer.drawShape(new me.Polygon(0, 0, [
-              new me.Vector2d(0, 0),
-              new me.Vector2d(0, 20),
-              new me.Vector2d(40, 20),
-              new me.Vector2d(40, 40),
-              new me.Vector2d(60, 40),
-              new me.Vector2d(60, 0)
+              new me.Vector2d(20, 0),
+              new me.Vector2d(40, 0),
+              new me.Vector2d(40, 60),
+              new me.Vector2d(0, 60),
+              new me.Vector2d(0, 40),
+              new me.Vector2d(20, 40)
             ]));
-            // var color = renderer.getColor();
-            // renderer.setColor('#5EFF7E');
-            // renderer.fillRect(0, 0, this.width, this.height);
-            // renderer.setColor(color);
         }
     }));
+    // this.alwaysUpdate = true;
+
+
+    this.velx = 450;
+    this.maxX = me.game.viewport.width - this.width;
+  },
+
+  update : function (time) {
+    if (me.input.isKeyPressed("left")) {
+        this.pos.x -= this.velx * time / 1000;
+    }
+
+    if (me.input.isKeyPressed("right")) {
+        this.pos.x += this.velx * time / 1000;
+    }
+
+
+    this.pos.x = this.pos.x.clamp(0, this.maxX);
+
+    return true;
+  },
+
+  onActivateEvent : function() {
+    var _this = this;
+    this.dropTimer = me.timer.setInterval(function() {
+      _this.pos.y += _this.dropVelocity;
+
+    }, 1000);
+  },
+
+  onDeactivateEvent : function() {
+    me.timer.clearInterval(this.timer);
   }
 });
--- js/screens/play.js
+++ js/screens/play.js
@@ -11,12 +11,23 @@ game.PlayScreen = me.ScreenObject.extend({
         this.HUD = new game.HUD.Container();
         me.game.world.addChild(this.HUD);
         me.game.world.addChild(me.pool.pull("block", 60, 60));
+
+        me.input.bindKey(me.input.KEY.LEFT, "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.A, "left");
+        me.input.bindKey(me.input.KEY.D, "right");
     },
 
     /**
      *  action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function() {
+
+        me.input.unbindKey(me.input.KEY.LEFT);
+        me.input.unbindKey(me.input.KEY.RIGHT);
+        me.input.unbindKey(me.input.KEY.A);
+        me.input.unbindKey(me.input.KEY.D);
+        
         // remove the HUD from the game world
         me.game.world.removeChild(this.HUD);
     }
