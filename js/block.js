61c2bf47b70ee867f1cc4f4bc1bed5e9be588adc
talsu <talsu84@gmail.com>
2018-07-12 00:02:11 +0900

    Create J, L, O, S, T, Z block.

--- js/block.js
+++ js/block.js
@@ -2,67 +2,13 @@ game.Block = me.Entity.extend({
   init : function (x, y) {
     this._super(me.Entity, "init", [0, 0, { width: 60, height: 60 }]);
 
-    this.dots = {
-      I: {
-        0:[[0,1],[1,1],[2,1],[3,1]],
-        R:[[2,0],[2,1],[2,2],[2,3]],
-        2:[[0,2],[1,2],[2,2],[3,2]],
-        L:[[1,0],[1,1],[1,2],[1,3]]
-      }
-    };
-    this.I_KickData = {
-      "0>R":  [[0,0],	[-2,0],	[+1,0],	[-2,-1],	[+1,+2]],
-      "R>0":	[[0,0],	[+2,0],	[-1,0],	[+2,+1],	[-1,-2]],
-      "R>2":	[[0,0],	[-1,0],	[+2,0],	[-1,+2],	[+2,-1]],
-      "2>R":	[[0,0],	[+1,0],	[-2,0],	[+1,-2],	[-2,+1]],
-      "2>L":	[[0,0],	[+2,0],	[-1,0],	[+2,+1],	[-1,-2]],
-      "L>2":	[[0,0],	[-2,0],	[+1,0],	[-2,-1],	[+1,+2]],
-      "L>0":	[[0,0],	[+1,0],	[-2,0],	[+1,-2],	[-2,+1]],
-      "0>L":	[[0,0],	[-1,0],	[+2,0],	[-1,+2],	[+2,-1]],
-    };
-    this.blockType = 'I';
+    this.blockType = 'Z';
     this.rotateType = '0';
-    //this.dotArray = [{row:0, col:1}, {row:1, col:1}, {row:2, col:1}, {row:2, col:0}]; // J
 
     this.move(0, 0);
-
-    this.dropVelocity = 20;
-
-    // this.renderable = new (me.Renderable.extend({
-    //     init : function () {
-    //         this._super(me.Renderable, "init", [0, 0, 60, 60]);
-    //     },
-    //     destroy : function () {},
-    //     draw : function (renderer) {
-    //         var color = renderer.getColor();
-    //         renderer.setColor('#5EFF7E');
-    //         renderer.drawShape(new me.Polygon(0, 0, [
-    //           new me.Vector2d(20, 0),
-    //           new me.Vector2d(40, 0),
-    //           new me.Vector2d(40, 60),
-    //           new me.Vector2d(0, 60),
-    //           new me.Vector2d(0, 40),
-    //           new me.Vector2d(20, 40)
-    //         ]));
-    //         renderer.setColor(color);
-    //
-    //     },
-    //     update: function() {
-    //       return false;
-    //     }
-    // }));
-    // this.alwaysUpdate = true;
-
-
-    this.velx = 450;
-    this.maxX = me.game.viewport.width - this.width;
-
-    this.keyPressScala = {
-      left:0,right:0,drop:0,down:0,anticlockwise:0,clockwise:0
-    };
   },
-  getDotArray(){
-    return this.dots[this.blockType][this.rotateType];
+  getDotArray() {
+    return game.Block.DOTS[this.blockType][this.rotateType];
   },
   rotate(isClockwise) {
     var types = ['0', 'R', '2', 'L'];
@@ -70,13 +16,15 @@ game.Block = me.Entity.extend({
     index += isClockwise ? 1 : -1;
     index = (types.length + index) % types.length;
     var newRotateType = types[index];
-    
+
     var kickData = null;
-    if (this.blockType == "I") kickData = this.I_KickData[this.rotateType + '>' + newRotateType];
+    if (this.blockType == "O") return true;
+    else if (this.blockType == "I") kickData = game.Block.I_KICK_DATA[this.rotateType + '>' + newRotateType];
+    else kickData = game.Block.JLSTZ_KICK_DATA[this.rotateType + '>' + newRotateType];
 
     var rotateSuccess = false;
     for (var i = 0; i < kickData.length; ++i) {
-      console.log(i);
+      // console.log(i);
       var newCol = this.col + kickData[i][0];
       var newRow = this.row + kickData[i][1];
       if (this.isValidPosition(newRotateType, newRow, newCol)) {
@@ -91,7 +39,7 @@ game.Block = me.Entity.extend({
   },
   draw : function (renderer) {
       var color = renderer.getColor();
-      renderer.setColor('#5EFF7E');
+      renderer.setColor(game.Block.COLOR[this.blockType]);
       this.getDotArray().forEach(function(colRow) {
         renderer.fillRect(
           colRow[0] * game.PlayField.BLOCK_SIZE,
@@ -99,23 +47,14 @@ game.Block = me.Entity.extend({
           game.PlayField.BLOCK_SIZE,
           game.PlayField.BLOCK_SIZE);
       });
-      // renderer.drawShape(new me.Polygon(0, 0, [
-      //   new me.Vector2d(20, 0),
-      //   new me.Vector2d(40, 0),
-      //   new me.Vector2d(40, 60),
-      //   new me.Vector2d(0, 60),
-      //   new me.Vector2d(0, 40),
-      //   new me.Vector2d(20, 40)
-      // ]));
       renderer.setColor(color);
 
   },
   isValidPosition: function(type, row, col) {
-    var dotArray = this.dots[this.blockType][type];
+    var dotArray = game.Block.DOTS[this.blockType][type];
     for (var i = 0;i < dotArray.length; ++i) {
       var newRow = row + dotArray[i][1];
       var newCol = col + dotArray[i][0];
-      console.log({i:i,row:newRow,col:newCol});
       if (newRow < 0 || game.PlayField.ROW_COUNT <= newRow) return false; // cannot setPosition
       if (newCol < 0 || game.PlayField.COL_COUNT <= newCol) return false; // cannot setPosition
     }
@@ -123,65 +62,97 @@ game.Block = me.Entity.extend({
     return true;
   },
   move: function(row, col) {
-    if (!this.isValidPosition(this.rotateType, row, col)) return;
+    if (!this.isValidPosition(this.rotateType, row, col)) return false;
 
     this.row = row;
     this.col = col;
 
     this.pos.x = this.col * game.PlayField.BLOCK_SIZE;
     this.pos.y = this.row * game.PlayField.BLOCK_SIZE;
-  },
-  update : function (time) {
-    if (me.input.isKeyPressed("left")) {
-      if (!this.keyPressScala.left) this.move(this.row, this.col - 1);
-      this.keyPressScala.left += time;
-    }
-
-    if (me.input.isKeyPressed("right")) {
-      if (!this.keyPressScala.right) this.move(this.row, this.col + 1);
-      this.keyPressScala.right += time;
-    }
-
-    if (me.input.isKeyPressed("down")) {
-      if (!this.keyPressScala.down) this.move(this.row + 1, this.col);
-      this.keyPressScala.down += time;
-    }
-
-    if (me.input.isKeyPressed("drop")) {
-      if (!this.keyPressScala.drop) this.move(this.row - 1, this.col);
-      this.keyPressScala.drop += time;
-    }
-
-    if (me.input.isKeyPressed("clockwise")) {
-      if (!this.keyPressScala.clockwise) this.rotate(true);
-      this.keyPressScala.clockwise += time;
-    }
-
-    if (me.input.isKeyPressed("anticlockwise")) {
-      if (!this.keyPressScala.anticlockwise) this.rotate(false);
-      this.keyPressScala.anticlockwise += time;
-    }
-
-    if (this.keyPressScala.left > 140 || !me.input.keyStatus('left')) this.keyPressScala.left = 0;
-    if (this.keyPressScala.right > 140 || !me.input.keyStatus('right')) this.keyPressScala.right = 0;
-    if (this.keyPressScala.down > 140 || !me.input.keyStatus('down')) this.keyPressScala.down = 0;
-    if (this.keyPressScala.drop > 140 || !me.input.keyStatus('drop')) this.keyPressScala.drop = 0;
-
-    if (this.keyPressScala.clockwise > 200 || !me.input.keyStatus('clockwise')) this.keyPressScala.clockwise = 0;
-    if (this.keyPressScala.anticlockwise > 200 || !me.input.keyStatus('anticlockwise')) this.keyPressScala.anticlockwise = 0;
 
     return true;
   },
 
-  onActivateEvent : function() {
-    // var _this = this;
-    // this.dropTimer = me.timer.setInterval(function() {
-    //   _this.move(_this.row + 1, _this.col);
-    //
-    // }, 1000);
-  },
+  moveLeft: function() { return this.move(this.row, this.col - 1); },
+  moveRight: function() { return this.move(this.row, this.col + 1); },
+  moveUp: function() { return this.move(this.row - 1, this.col); },
+  moveDown: function() { return this.move(this.row + 1, this.col); },
 
-  onDeactivateEvent : function() {
-    // me.timer.clearInterval(this.timer);
-  }
 });
+
+game.Block.DOTS = {
+  I: {
+    0:[[0,1],[1,1],[2,1],[3,1]],
+    R:[[2,0],[2,1],[2,2],[2,3]],
+    2:[[0,2],[1,2],[2,2],[3,2]],
+    L:[[1,0],[1,1],[1,2],[1,3]]
+  },
+  J: {
+    0:[[0,0],[0,1],[1,1],[2,1]],
+    R:[[1,0],[2,0],[1,1],[1,2]],
+    2:[[0,1],[1,1],[2,1],[2,2]],
+    L:[[1,0],[1,1],[0,2],[1,2]]
+  },
+  L: {
+    0:[[2,0],[0,1],[1,1],[2,1]],
+    R:[[1,0],[2,2],[1,1],[1,2]],
+    2:[[0,1],[1,1],[2,1],[0,2]],
+    L:[[1,0],[1,1],[0,0],[1,2]]
+  },
+  O: {
+    0:[[1,0],[2,0],[1,1],[2,1]],
+    R:[[1,0],[2,0],[1,1],[2,1]],
+    2:[[1,0],[2,0],[1,1],[2,1]],
+    L:[[1,0],[2,0],[1,1],[2,1]]
+  },
+  S: {
+    0:[[1,0],[2,0],[0,1],[1,1]],
+    R:[[1,0],[1,1],[2,1],[2,2]],
+    2:[[1,1],[2,1],[0,2],[1,2]],
+    L:[[0,0],[0,1],[1,1],[1,2]]
+  },
+  T: {
+    0:[[1,0],[0,1],[1,1],[2,1]],
+    R:[[1,0],[1,1],[2,1],[1,2]],
+    2:[[0,1],[1,1],[2,1],[1,2]],
+    L:[[1,0],[0,1],[1,1],[1,2]]
+  },
+  Z: {
+    0:[[0,0],[1,0],[1,1],[2,1]],
+    R:[[2,0],[1,1],[2,1],[1,2]],
+    2:[[0,1],[1,1],[2,2],[1,2]],
+    L:[[1,0],[0,1],[1,1],[0,2]]
+  }
+};
+
+game.Block.COLOR = {
+  I:"cyan",
+  J:"blue",
+  L:"orange",
+  T:"purple",
+  O:"yellow",
+  Z:"red",
+  S:"green"
+};
+
+game.Block.I_KICK_DATA = {
+  "0>R":  [[0,0],	[-2,0],	[+1,0],	[-2,-1],	[+1,+2]],
+  "R>0":	[[0,0],	[+2,0],	[-1,0],	[+2,+1],	[-1,-2]],
+  "R>2":	[[0,0],	[-1,0],	[+2,0],	[-1,+2],	[+2,-1]],
+  "2>R":	[[0,0],	[+1,0],	[-2,0],	[+1,-2],	[-2,+1]],
+  "2>L":	[[0,0],	[+2,0],	[-1,0],	[+2,+1],	[-1,-2]],
+  "L>2":	[[0,0],	[-2,0],	[+1,0],	[-2,-1],	[+1,+2]],
+  "L>0":	[[0,0],	[+1,0],	[-2,0],	[+1,-2],	[-2,+1]],
+  "0>L":	[[0,0],	[-1,0],	[+2,0],	[-1,+2],	[+2,-1]]
+};
+
+game.Block.JLSTZ_KICK_DATA = {
+  "0>R":	[[ 0, 0],	[-1, 0],	[-1,+1],	[ 0,-2],	[-1,-2]],
+  "R>0":	[[ 0, 0],	[+1, 0],	[+1,-1],	[ 0,+2],	[+1,+2]],
+  "R>2":	[[ 0, 0],	[+1, 0],	[+1,-1],	[ 0,+2],	[+1,+2]],
+  "2>R":	[[ 0, 0],	[-1, 0],	[-1,+1],	[ 0,-2],	[-1,-2]],
+  "2>L":	[[ 0, 0],	[+1, 0],	[+1,+1],	[ 0,-2],	[+1,-2]],
+  "L>2":	[[ 0, 0],	[-1, 0],	[-1,-1],	[ 0,+2],	[-1,+2]],
+  "L>0":	[[ 0, 0],	[-1, 0],	[-1,-1],	[ 0,+2],	[-1,+2]],
+  "0>L":	[[ 0, 0],	[+1, 0],	[+1,+1],	[ 0,-2],	[+1,-2]]
+};
--- js/play-field.js
+++ js/play-field.js
@@ -8,8 +8,8 @@ game.PlayField = me.Container.extend({
       game.PlayField.COL_COUNT * game.PlayField.BLOCK_SIZE,
       game.PlayField.ROW_COUNT * game.PlayField.BLOCK_SIZE
     ]);
-
-    this.addChild(me.pool.pull("block", 0, 0));
+    this.activeBlock = me.pool.pull("block", 0, 0);
+    this.addChild(this.activeBlock);
 
     // this.addChild(new (me.Renderable.extend({
     //         init : function () {
@@ -25,6 +25,9 @@ game.PlayField = me.Container.extend({
     //     })));
 
 
+    this.keyPressScala = {
+      left:0,right:0,drop:0,down:0,anticlockwise:0,clockwise:0
+    };
   },
 
   draw: function(renderer) {
@@ -35,7 +38,66 @@ game.PlayField = me.Container.extend({
 
     this._super(me.Container, "draw", [renderer]);
   },
-  
+
+  update: function (time) {
+    this._super(me.Container, "update", [time]);
+
+    if (me.input.isKeyPressed("left")) {
+      if (!this.keyPressScala.left) this.activeBlock.moveLeft();
+      this.keyPressScala.left += time;
+    }
+
+    if (me.input.isKeyPressed("right")) {
+      if (!this.keyPressScala.right) this.activeBlock.moveRight();
+      this.keyPressScala.right += time;
+    }
+
+    if (me.input.isKeyPressed("down")) {
+      if (!this.keyPressScala.down) this.activeBlock.moveDown();
+      this.keyPressScala.down += time;
+    }
+
+    if (me.input.isKeyPressed("drop")) {
+      if (!this.keyPressScala.drop) this.activeBlock.moveUp();
+      this.keyPressScala.drop += time;
+    }
+
+    if (me.input.isKeyPressed("clockwise")) {
+      if (!this.keyPressScala.clockwise) this.activeBlock.rotate(true);
+      this.keyPressScala.clockwise += time;
+    }
+
+    if (me.input.isKeyPressed("anticlockwise")) {
+      if (!this.keyPressScala.anticlockwise) this.activeBlock.rotate(false);
+      this.keyPressScala.anticlockwise += time;
+    }
+
+    if (this.keyPressScala.left > 140 || !me.input.keyStatus('left')) this.keyPressScala.left = 0;
+    if (this.keyPressScala.right > 140 || !me.input.keyStatus('right')) this.keyPressScala.right = 0;
+    if (this.keyPressScala.down > 140 || !me.input.keyStatus('down')) this.keyPressScala.down = 0;
+    if (this.keyPressScala.drop > 140 || !me.input.keyStatus('drop')) this.keyPressScala.drop = 0;
+
+    if (this.keyPressScala.clockwise > 200 || !me.input.keyStatus('clockwise')) this.keyPressScala.clockwise = 0;
+    if (this.keyPressScala.anticlockwise > 200 || !me.input.keyStatus('anticlockwise')) this.keyPressScala.anticlockwise = 0;
+
+    return true;
+  },
+
+  onActivateEvent : function() {
+    this._super(me.Container, "onActivateEvent",[]);
+    var _this = this;
+    this.dropTimer = me.timer.setInterval(function() {
+      if (!_this.activeBlock.moveDown()) {
+        // [TODO] 비활성화 블럭 전환 대기 타이머 동작
+      }
+
+    }, 1000);
+  },
+
+  onDeactivateEvent : function() {
+    this._super(me.Container, "onDeactivateEvent",[]);
+    me.timer.clearInterval(this.timer);
+  }
 });
 
 game.PlayField.BLOCK_SIZE = 20;
